b2b6042e4ce20b156eda214e7d481808b746dc1f
Ester-Lusi <stlusi1254@gmail.com>
2026-03-08 15:06:18 +0200

    Refactor LoginUser class to record type with attributes

--- Dtos/LoginUser.cs
+++ Dtos/LoginUser.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace Dtos
 {
-    public class LoginUser
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+    public record LoginUser
+    (
+        [EmailAddress]
+        [Required]
+        string Email,
+
+        [Required]
+        string Password
+    );
 }
